486622c1de98e27e05af8dfb7a3fd39b75d65e38
unknown <nafim191119@gmai.com>
2023-03-30 13:13:28 +0600

    card-item-added

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -3,8 +3,7 @@ import './Product.css'
 
 const Product = (props) => {
     console.log(props.product)
-    const {img, name, seller, quantity, price, ratings
-    } = props.product;
+    const {img, name, seller, price, ratings} = props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +11,9 @@ const Product = (props) => {
             <p>Price: ${price}</p>
             <p>Manufacturer: {seller}</p>
             <p>Ratings: {ratings} Star</p>
+            <button className='btn-cart'>Add to Cart</button>
         </div>
+        
     );
 };
 
